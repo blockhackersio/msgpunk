91b5f5183266f57ce7f3a48a3a4ea73856f98f7d
ryardley <1256409+ryardley@users.noreply.github.com>
2026-09-04 11:08:12 +0000

    remove unused code

--- to-integrate/openform-player/build.sh
+++
@@ -1,2 +0,0 @@
-./node_modules/.bin/vite build 2>&1
-echo "BUILD_EXIT_CODE=$?"
--- to-integrate/openform-player/install.sh
+++
@@ -1,3 +0,0 @@
-cd /mnt/data/test-openform/openform-player
-pnpm add -D tailwindcss @tailwindcss/postcss postcss
-echo "INSTALL_DONE"
